292b2ac08580c7e5ad62776bb74688862a0692c1
VranaPavel <pavel.vrana@protonmail.com>
2024-08-25 14:00:37 +0200

    algorithmization

---
+++ algorithmization/hello_world/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Hello_world
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            while(true)
+            {
+                Console.Write("Please write down your name: ");
+                string? s = Console.ReadLine();
+
+                /* if (s == "Jana" || s == "Honza" || s == "Eva" || s == "Tomáš")
+                {
+                    Console.WriteLine("Hello {0}!", s);
+                }
+                else
+                {
+                    Console.WriteLine("Hello, I don't know who you are!");
+                } */
+
+                if (s == null)
+                {
+                    Console.WriteLine("Hello, I don't know who you are!");
+                }
+                else if (names.Contains(s))
+                {
+                    Console.WriteLine("Hello {0}!", s);
+                }
+                else
+                {
+                    Console.WriteLine("Hello, I don't know who you are!");
+                    names.Add(s);
+                }
+            }
+        }
+    }
+}
---
+++ algorithmization/statements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Statements
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+        start:
+            Console.Write("{0},", i);
+            i++;
+            if (i < 10)
+                goto start;
+            else
+                Console.WriteLine(i);
+
+            int j = 10;
+        second_start:
+            if (j % 2 == 1)
+                Console.WriteLine(j);
+            j--;
+            if (j > 0)
+                goto second_start;
+        }
+    }
+}
