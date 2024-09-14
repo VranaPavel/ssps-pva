7b90b5282aacb744e8412bde32e2c51b7c556905
CrowTheDev <pavel.vrana@protonmail.com>
2024-09-14 21:17:46 +0200

    String split

---
+++ string_split/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringSplit
+{
+    public class Program
+    {
+        public static int NumberOfWords(string input, char[] separators) {
+            string[] arr = input.Split(separators ,StringSplitOptions.RemoveEmptyEntries);
+            return arr.Length;
+        }
+        
+        public static void Main()
+        {
+            Console.WriteLine(NumberOfWords("Nebe je modré, nebude pršet.", new char[] {' ', ',', '.'}));
+        }
+    }
+}
